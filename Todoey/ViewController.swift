29d42f8dd2c25cc0ea43a5abed74261e862f4838
Julian Gurdan <juliangurdan@macbook-pro.local>
2018-09-18 19:26:20 +0200

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Julian Gurdan on 18.09.18.
+//  Copyright © 2018 Julian Gurdan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
