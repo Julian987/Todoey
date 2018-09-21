23d5e1f97298e466d9a5b8c37d6bcdaa09ae0f8f
Julian Gurdan <juliangurdan@macbook-pro.local>
2018-09-21 18:50:20 +0200

    Just added one comment and made all comments look nice ^^

--- Todoey/AppDelegate.swift
+++ Todoey/AppDelegate.swift
@@ -9,16 +9,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
 
-    //Gets called first. Immediatly after the app launched:
+    /*Gets called first. Immediatly after the app launched:*/
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        /* Override point for customization after application launch.*/
         print("didFinishLaunchingWithOptions")
         
                 
         return true
     }
     
-    //App is going to be terminated (can be user triggered or system triggered)
+    /*App is going to be terminated (can be user triggered or system triggered)*/
     func applicationWillTerminate(_ application: UIApplication) {
 
         self.saveContext()
@@ -26,17 +26,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     // MARK: - Core Data stack
     
-    //lazy == only gets loaded up with a vaule when its needed:
-    //The NSPersistentContainer is where we are going to store all of our Data:
-    //the default PersistentContainer is a SQL like Database:
+    /*lazy == only gets loaded up with a vaule when its needed:
+      The NSPersistentContainer is where we are going to store all of our Data:
+      the default PersistentContainer is a SQL like Database:*/
     lazy var persistentContainer: NSPersistentContainer = {
 
-        // now we create a constant called container that sets up this new persistent container with
-        // the name of our data model. (Data Model is DataModel.xcdatamodeld)
-        // these to names HAVE TO match
+        /* now we create a constant called container that sets up this new persistent container with
+           the name of our data model. (Data Model is DataModel.xcdatamodeld)
+           these to names HAVE TO match*/
         let container = NSPersistentContainer(name: "DataModel")
         
-        //Here were going to load up this persistent Store:
+        /*Here were going to load up this persistent Store:*/
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
 
@@ -45,19 +45,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         })
         
         
-        //if there weren't any errors we are going to return the container.
-        //then our persistentContainer contains this NSPersistentContainer Object.
+        /*if there weren't any errors we are going to return the container.
+          then our persistentContainer contains this NSPersistentContainer Object.*/
         return container
     }()
     
     // MARK: - Core Data Saving support
     
+    /*We just need this method to call it in applicationWillTerminate to save everything that we did in our app bevor terminating.*/
     func saveContext () {
-        //essentially the context is an area where you can change and update your data so you can undo
-        //and redo until you're happy with your data.
-        //then you can save the data that's in your contacts (also called your temporary area) to the
-        //container which is for permanent storage.
-        //Simular to the staging area in github (diese zwischen area halt.)
+        /*essentially the context is an area where you can change and update your data so you can undo
+          and redo until you're happy with your data.
+          then you can save the data that's in your contacts (also called your temporary area) to the
+          container which is for permanent storage.
+          Simular to the staging area in github (diese zwischen area halt.)*/
         let context = persistentContainer.viewContext
         if context.hasChanges {
             do {
